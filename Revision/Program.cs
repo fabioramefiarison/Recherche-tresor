16fab08b3e9f53f3f71f237209c08a3a53b3626c
fabioramefiarison <fabioramefiarison@gmail.com>
2024-07-12 18:12:16 +0300

    first commit

---
+++ Revision/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Revision
+{
+    class Program
+    {
+        public static void SeDeplacer(int initialX, int initialY, string chemin)
+        {
+            foreach(char c in chemin)
+            {
+                if (c == 'N' && initialY > 0) initialY -= 1;
+                if (c == 'S' && initialY < 3) initialY += 1;
+                if (c == 'E' && initialX < 4) initialX += 1;
+                if (c == 'O' && initialX > 0) initialX -= 1;
+            }
+            Console.WriteLine("x = {0} et y = {1}",initialX, initialY);
+        }
+
+        public static string TrouverTresor(int initialX, int initialY, int tresorX, int tresorY)
+        {
+            string resultat = "";
+            int count = 0;
+            while(initialX != tresorX || initialY != tresorY)
+            {
+                count++;
+                if (initialX < tresorX)
+                {
+                    initialX += 1;
+                    resultat += "E";
+                }
+                if (initialX > tresorX)
+                {
+                    initialX -= 1;
+                    resultat += "O";
+                }
+                if (initialY > tresorY)
+                {
+                    initialY -= 1;
+                    resultat += "N";
+                }
+                if (initialY < tresorY)
+                {
+                    initialY += 1;
+                    resultat += "S";
+                }
+            }
+            Console.WriteLine("sans else " + count);
+            return resultat;
+        }
+
+        public static string TrouverTresor2(int initialX, int initialY, int tresorX, int tresorY)
+        {
+            string resultat = "";
+            int count = 0;
+            while (initialX != tresorX || initialY != tresorY)
+            {
+                count++;
+                if (initialX < tresorX)
+                {
+                    initialX += 1;
+                    resultat += "E";
+                }
+                else if (initialX > tresorX)
+                {
+                    initialX -= 1;
+                    resultat += "O";
+                }
+                else if (initialY > tresorY)
+                {
+                    initialY -= 1;
+                    resultat += "N";
+                }
+                else if (initialY < tresorY)
+                {
+                    initialY += 1;
+                    resultat += "S";
+                }
+            }
+            Console.WriteLine("avec else " + count);
+            return resultat;
+        }
+        static void Main(string[] args)
+        {
+
+            Point p1 = new Point(1, 1);
+            Point p2 = new Point(4, 2);
+            Terrain t1 = new Terrain(4, 3, p1, p2);
+            Console.WriteLine(t1.TrouverTresor());
+        }
+    }
+}
---
+++ Revision/Terrain.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Revision
+{
+    class Terrain
+    {
+        public int DimensionX { get; set; }
+        public int DimensionY { get; set; }
+        public Point PointInitial { get; set; }
+        public Point Tresor { get; set; }
+
+        public Terrain(int x, int y, Point initial, Point tresor)
+        {
+            this.DimensionX = x;
+            this.DimensionY = y;
+            this.PointInitial = initial;
+            this.Tresor = tresor;
+        }
+
+        public string TrouverTresor()
+        {
+            string resultat = "";
+            int count = 0;
+            while (this.PointInitial.X != this.Tresor.X || this.PointInitial.Y != this.Tresor.Y)
+            {
+                count++;
+                if (this.PointInitial.X < this.Tresor.X)
+                {
+                    this.PointInitial.X += 1;
+                    resultat += "E";
+                }
+                if (this.PointInitial.X > this.Tresor.X)
+                {
+                    this.PointInitial.X -= 1;
+                    resultat += "O";
+                }
+                if (this.PointInitial.Y > this.Tresor.Y)
+                {
+                    this.PointInitial.Y -= 1;
+                    resultat += "N";
+                }
+                if (this.PointInitial.Y < this.Tresor.Y)
+                {
+                    this.PointInitial.Y += 1;
+                    resultat += "S";
+                }
+            }
+            Console.WriteLine("sans else " + count);
+            return resultat;
+        }
+
+
+    }
+}
